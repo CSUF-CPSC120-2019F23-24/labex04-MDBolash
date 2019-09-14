61e93d04467acff889a446f5674cc7222b5a684c
MD_Bolash <md_bolash@cs104u-1c.acad.fullerton.edu>
2019-09-14 14:00:22 -0700

    Sales prediction done

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,16 @@
 // Sales prediction
+#include <iostream>
+
+int main ()
+{
+long total_sales2;
+const long total_sales = 2103419277;
+const double INCREASE = 0.18;
+//Display last years Sales
+std::cout << "Last years sales amounted to " << total_sales <<  '\n';
+
+//Calculate predictions for next year.
+total_sales2 = total_sales + (total_sales*INCREASE) ;
+//Display next years predicted sales
+std::cout << "This years sales may amount to " << static_cast <long long>(total_sales2) << " ." ;
+}
