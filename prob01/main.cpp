26d0a13d7c135c12e859392ccb6ff2056b44d08d
MD_Bolash <md_bolash@cs104u-1c.acad.fullerton.edu>
2019-09-14 13:29:21 -0700

    Prob 01 done

--- prob01/main.cpp
+++ prob01/main.cpp
@@ -8,28 +8,30 @@ int main()
   // then print both the char and the short
 
   char my_char1 = 'J';
-  short my_short1 = ;
-  std::cout << "The ASCII value for " << my_char1 << " is " << my_short1 << std::endl;
+  short my_short1 = 74 ;
+  std::cout << "The ASCII value for " << my_short1 << " is " << static_cast <short>(my_char1) << '\n';
 
-  std::cout << std::endl; // blank line to separate output from the different exercises
+  std::cout << my_char1 << '\n'; // blank line to separate output from the different exercises
 
   // TODO#2: add one to the character to see what gets printed out
 
   char my_char2 = 'C';
-  my_char2 = ;
-  std::cout << "Char1: " << my_char2 << std::endl;
+  my_char2 = 67 ;
+  my_char2 = my_char2 + 1 ;
+  std::cout << "Char1: " << my_char2 << '\n';
 
   std::cout << std::endl; // blank line to separate output from the different exercises
 
   // TODO#3: again add one to the character to see what gets printed out
   // and add a comment explaining why the value of the character is no
   // longer alphabetic
-
+  /* The character is no longer alphabetic because char is reading an increase by 1 from z
+  , since z is the last letter of the alphabet it increases to a numerical value. */
   char my_char3 = 'z';
-  my_char3 = ;
-  std::cout << "Char2: " << my_char3 << std::endl;
+  my_char3 = my_char3+1 ;
+  std::cout << "Char3: " << my_char3 << '\n';
 
-  std::cout << std::endl; // blank line to separate output from the different exercises
+  std::cout << '\n'; // blank line to separate output from the different exercises
 
   // TODO#4: print each number, then add one to each number and print it again
   // also add a comment to explain why my_short2 value became negative after
@@ -42,13 +44,15 @@ int main()
 
   std::cout << "Short (" << sizeof(my_short2) << " bytes): "
     << std::dec << my_short2 << std::hex << " (0x" << my_short2 << ')';
-  my_short2 = ;
+  my_short2 = my_short2+1;
+
   std::cout << ", after adding 1: " << std::dec << my_short2
     << std::hex << " (0x" << my_short2 << ")\n";
 
   std::cout << "Int (" << sizeof(my_short2) << " bytes): "
     << std::dec << my_int2 << std::hex << " (0x" << my_int2 << ')';
-  my_int2 = ;
+
+  my_int2 = my_int2+1 ;
   std::cout << ", after adding 1: " << std::dec << my_int2
     << std::hex << " (0x" << my_int2 << ")\n";
 
@@ -60,16 +64,18 @@ int main()
 
   unsigned short my_short3 = 65535; // maximum value for an unsigned short
   int my_int3 = my_short3;
-
+ /* my_short2 became negative after adding 1 because 32767 is the max positive value for short,
+ leading to a overflow error. my_int2 did not become negative because an unsigned int can store more positive values than
+ short because it can store only positives and does not store negatives. */
   std::cout << "Unsigned short (" << sizeof(my_short3) << " bytes): "
     << std::dec << my_short3 << std::hex << " (0x" << my_short3 << ')';
-  my_short3 = ;
+  my_short3 = my_short3+1 ;
   std::cout << ", after adding 1: " << std::dec << my_short3
     << std::hex << " (0x" << my_short3 << ")\n";
 
   std::cout << "Same value as int (" << sizeof(my_int3) << " bytes): "
     << std::dec << my_int3 << std::hex << " (0x" << my_int3 << ')';
-  my_int3 = ;
+  my_int3 = my_int3 + 1;
   std::cout << ", after adding 1: " << std::dec << my_int3
     << std::hex << " (0x" << my_int3 << ")\n";
 
